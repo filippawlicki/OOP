02c5813533fd42eaeb006b74feb92744116f02bb
filippawlicki <filip.pawlicki@gmail.com>
2024-02-28 20:26:09 +0100

    Done Laboratory in Home Part

--- LAB01/main.cpp
+++ LAB01/main.cpp
@@ -22,24 +22,33 @@ void Funkcja1(){
   cout << "Pole: " << prostWsk->Pole() << endl;
   cout << "Obwod: " << prostWsk->Obwod() << endl;
   delete prostWsk; // Zwolnienie pamieci
-}
+} // Destruktor prost uruchamia sie po wyjsciu z funkcji
 
-void Funkcja2()
-{
+void Funkcja2(){ // Wirtualne bo sa nadpisywane
 	FiguraPlaska* tab[3] = { new Prostokat(6,2), new Trojkat(12, 17, 15), new Kolo(8) };
 	for(int i = 0; i < 3; i++)
 	{
-		cout << "Obwod figury " << i + 1 << ": " << tab[i]->Obwod() << endl;
-		cout << "Pole figury " << i + 1 << ": " << tab[i]->Pole() << endl;
+		cout << "Obwod figury " << i + 1 << ": " << tab[i]->Obwod() << endl; // jakby nie byly wirtualne
+		cout << "Pole figury " << i + 1 << ": " << tab[i]->Pole() << endl; // to zostalaby uruchomione metody dla FiguraPlaska
 		delete tab[i]; // Zwolnienie pamieci
 	}
 	cout << endl;
 }
 
+int Funkcja3(int x, int y){ // overloading
+  return x + y;
+}
+
+int Funkcja3(int x, int y, int z){ // overloading
+  return x + y + z;
+}
+// overriding jest uzywany w kazdej figurze w funkcji wypisz -> nadpisuje metode
 
 
 int main(){
-  // Funkcja1();
-  Funkcja2();
+  //Funkcja1();
+  //Funkcja2();
+  //cout << "Podpunkt 3, pierwsze wyjscie: " << Funkcja3(1, 2) << endl;
+  //cout << "Podpunkt 3, drugie wyjscie: " << Funkcja3(1, 2, 3) << endl;
   return 0;
 }
